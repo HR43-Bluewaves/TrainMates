d9b1d52e0f36975c44a7db64eb526bc9f8877b9e
denniswang10 <denniswang2100g@gmail.com>
2021-05-22 15:07:22 -0700

    added seed test file

---
+++ server/database/index.js
@@ -0,0 +1,41 @@
+/* eslint-disable no-console */
+const { Pool } = require('pg');
+const config = require('../../config/config');
+
+// Local host version
+// Before running this script, make sure you have a database created called Blueocean
+const pool = new Pool({
+  user: 'postgres',
+  host: 'localhost',
+  database: 'Blueocean',
+  password: config.password,
+  port: 5432, // This is the default port
+});
+
+// // AWS server version
+// const pool = new Pool({
+//   user: config.AWSUSER,
+//   host: config.AWS,
+//   database: 'Reviews',
+//   password: config.AWSPW,
+//   port: 5432,
+// });
+
+// // AWS postgres version
+// const pool = new Pool({
+//   user: config.AWSUSER,
+//   host: 'localhost',
+//   database: 'Reviews',
+//   password: config.AWSPW,
+//   port: 5432,
+// });
+
+pool.connect((err, res) => {
+  if (err) {
+    throw err;
+  } else {
+    console.log('Connected to database');
+  }
+});
+
+module.exports = pool;
---
+++ server/database/seedUsers.js
@@ -0,0 +1,34 @@
+/* eslint-disable no-console */
+const { Client, Pool } = require('pg');
+const pool = require('./index');
+
+const tableName = 'users';
+const creatTable = `
+DROP TABLE IF EXISTS ${tableName};
+CREATE TABLE IF NOT EXISTS ${tableName} (
+  USER_ID SERIAL,
+  FIRST_NAME TEXT NOT NULL,
+  LAST_NAME TEXT NOT NULL,
+  GENDER TEXT,
+  EMAIL TEXT NOT NULL,
+  CITY TEXT NOT NULL,
+  STATE TEXT NOT NULL,
+  ZIP INT NOT NULL,
+  PHOTO_URL TEXT,
+  USER_NAME TEXT NOT NULL,
+  PASSWORD TEXT NOT NULL
+);
+`;
+
+const insertData = `
+  INSERT INTO ${tableName} (FIRST_NAME, LAST_NAME, GENDER, EMAIL, CITY, STATE, ZIP, PHOTO_URL, USER_NAME, PASSWORD) VALUES
+  ('Regina', 'George', 'Female', 'totallyfetch@gmail.com',
+   'Evanston', 'IL', 60201 ,'https://img.particlenews.com/image.php?type=thumbnail_580x000&url=3bRbfV_0XEGgX2j00',
+   'regina', 'regina');
+`;
+pool.query(creatTable)
+  .then(() => console.log(`Table '${tableName}' successfully created!`))
+  .then(() => {
+    pool.query(insertData).then(() => console.log('Get in loser, we\'re going shopping'));
+  })
+  .catch((err) => console.error('Error executing query', err.stack));
