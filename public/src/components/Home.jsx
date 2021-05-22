848fff36b78fcc63030d260deb17e6af2d4b7201
Michael Rosen <mrosen86@gmail.com>
2021-05-22 11:46:49 -0700

    cleaned up lint

--- public/src/components/Home.jsx
+++ public/src/components/Home.jsx
@@ -1,12 +1,10 @@
 import React from 'react';
 import { Button } from 'react-bootstrap';
 
-const Home = (props) => {
-  return (
-    <div>
-      <h1>This is the home page</h1>
-    </div>
-  );
-}
+const Home = () => (
+  <div>
+    <h1>This is the home page</h1>
+  </div>
+);
 
 export default Home;
