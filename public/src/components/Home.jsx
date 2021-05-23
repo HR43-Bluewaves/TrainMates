a81e3a7c32ba9471e9cf1cac1efb3ff7f19d13d1
Michael Rosen <mrosen86@gmail.com>
2021-05-22 18:05:30 -0700

    added css modules example

--- public/src/components/Home.jsx
+++ public/src/components/Home.jsx
@@ -1,8 +1,9 @@
 import React from 'react';
 import { Button } from 'react-bootstrap';
+import styles from './test.module.css';
 
 const Home = () => (
-  <div>
+  <div className={styles.div}>
     <h1>This is the home page</h1>
   </div>
 );
