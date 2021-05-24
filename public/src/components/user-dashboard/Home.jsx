de0039fe233ef47d5f9a77948b5e5490f49fb8bd
Nader <naderdamouni2@gmail.com>
2021-05-24 12:17:09 -0700

    initial routing

--- public/src/components/user-dashboard/Home.jsx
+++ public/src/components/user-dashboard/Home.jsx
@@ -1,9 +1,12 @@
 /* eslint-disable no-console */
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import styles from '../test.module.css';
 import NavBar from './Navbar';
 
 const Home = () => {
+  const [trainers, setTrainers] = useState([]);
+  const [classes, setClasses] = useState([]);
+
   return (
     <div className={styles.div}>
       <h1>This is the home page</h1>
