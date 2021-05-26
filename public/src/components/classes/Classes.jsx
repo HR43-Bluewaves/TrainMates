4d1547d38591381c4c78afa8eee5e97b39d2b429
Nader <naderdamouni2@gmail.com>
2021-05-26 12:00:17 -0700

    refactored trainer and classes navbar

--- public/src/components/classes/Classes.jsx
+++ public/src/components/classes/Classes.jsx
@@ -1,13 +1,7 @@
 /* eslint-disable react/jsx-props-no-spreading */
 import React, { useState } from 'react';
 import { useSelector } from 'react-redux';
-import { useHistory } from 'react-router-dom';
-import { Button, Nav } from 'react-bootstrap';
 import Container from 'react-bootstrap/Container';
-import ShoppingCartIcon from '@material-ui/icons/ShoppingCart';
-import EmailIcon from '@material-ui/icons/Email';
-import NotificationsIcon from '@material-ui/icons/Notifications';
-import PersonIcon from '@material-ui/icons/Person';
 import Autocomplete from '@material-ui/lab/Autocomplete';
 import TextField from '@material-ui/core/TextField';
 import ClassList from './ClassList';
@@ -15,7 +9,6 @@ import Navbar from '../user-dashboard/Navbar';
 import './class.css';
 
 const Classes = () => {
-  const history = useHistory();
   const [searchValue, setSearchValue] = useState('');
   const classes = useSelector((state) => state.classesReducer.classes);
 
@@ -25,34 +18,6 @@ const Classes = () => {
   // };
   const handleChange = (event, value) => {
     setSearchValue(value);
-  };
-
-  const handleHome = () => {
-    history.push('/home');
-  };
-
-  const handleTrainers = () => {
-    history.push('/trainers');
-  };
-
-  const handleClasses = () => {
-    history.push('/classes');
-  };
-
-  const handleCart = () => {
-    history.push('/cart');
-  };
-
-  const handleChat = () => {
-    history.push('/chat');
-  };
-
-  const handleNotifications = () => {
-    history.push('/notifications');
-  };
-
-  const handleUser = () => {
-    history.push('/user');
   };
 
   return (
--- public/src/components/trainers/Trainers.jsx
+++ public/src/components/trainers/Trainers.jsx
@@ -5,12 +5,6 @@
 /* eslint-disable no-console */
 import React, { useState } from 'react';
 import { useSelector } from 'react-redux';
-import { useHistory } from 'react-router-dom';
-import { Button, Nav } from 'react-bootstrap';
-import ShoppingCartIcon from '@material-ui/icons/ShoppingCart';
-import EmailIcon from '@material-ui/icons/Email';
-import NotificationsIcon from '@material-ui/icons/Notifications';
-import PersonIcon from '@material-ui/icons/Person';
 import Autocomplete from '@material-ui/lab/Autocomplete';
 import TextField from '@material-ui/core/TextField';
 import './trainer.css';
@@ -18,41 +12,12 @@ import TrainersList from './TrainersList';
 import Navbar from '../user-dashboard/Navbar';
 
 const Trainers = () => {
-  const history = useHistory();
   const [searchValue, setSearchValue] = useState('');
 
   const trainers = useSelector((state) => state.trainersReducer.trainers);
 
   const handleChange = (event, value) => {
     setSearchValue(value);
-  };
-
-  const handleHome = () => {
-    history.push('/home');
-  };
-
-  const handleTrainers = () => {
-    history.push('/trainers');
-  };
-
-  const handleClasses = () => {
-    history.push('/classes');
-  };
-
-  const handleCart = () => {
-    history.push('/cart');
-  };
-
-  const handleChat = () => {
-    history.push('/chat');
-  };
-
-  const handleNotifications = () => {
-    history.push('/notifications');
-  };
-
-  const handleUser = () => {
-    history.push('/user');
   };
 
   return (
