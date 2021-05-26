3da76f90166aa4b9aa6bf16781294d1746f1a16c
denniswang10 <denniswang2100g@gmail.com>
2021-05-25 20:43:13 -0700

    added testing route for class profile

---
+++ public/src/components/classes/FullClass.jsx
@@ -0,0 +1,11 @@
+import React, { useState } from 'react';
+
+const FullClass = () => {
+  return (
+    <div>
+      This is Full class detail page
+    </div>
+  );
+};
+
+export default FullClass;
