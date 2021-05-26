0ab708cbb8ae2c29b401f596619a4d78aff3ae15
B1-sauce <hby940604@gmail.com>
2021-05-26 10:37:46 -0700

    merge conflicts

--- public/src/components/userInformation/UserInformation.jsx
+++ public/src/components/userInformation/UserInformation.jsx
@@ -4,8 +4,13 @@ import { useSelector } from 'react-redux';
 import Container from 'react-bootstrap/Container';
 import Col from 'react-bootstrap/Col';
 import Row from 'react-bootstrap/Row';
-import NavBar from '../user-dashboard/Navbar';
 import './userInformation.css';
+import '../user-dashboard/home_nav.module.css';
+import RoomOutlinedIcon from '@material-ui/icons/RoomOutlined';
+import EmailOutlinedIcon from '@material-ui/icons/EmailOutlined';
+import ChatIcon from '@material-ui/icons/Chat';
+import EditIcon from '@material-ui/icons/Edit';
+import NavBar from '../user-dashboard/Navbar';
 import UserClasses from './userClasses';
 
 const UserInformation = () => {
@@ -14,43 +19,43 @@ const UserInformation = () => {
   const handleChange = (event) => {
     setSearchValue(event.target.value);
   };
-
   if (user) {
     return (
       <Container fluid className="userInformationPage">
         <NavBar />
         <Row>
-        <Col className="userContainer" sm={4} >
-          <Col className="userInformationContainer">
-            <Row className="photoContainer">
-              <img className="userPhoto" src={user.photo_url} alt="photos" />
-            </Row>
-            <Row className="userInformation">
-              <h3>
-                {user.first_name} {user.last_name}
-              </h3>
-              <p>
-              <RoomOutlinedIcon className="userIcons"/>
-                {user.city} {user.state}, {user.zip}
-              </p>
-              <p>
-                <EmailOutlinedIcon className="userIcons"/>
-                {user.email}
-              </p>
+          <Col className="userContainer" sm={4}>
+            <Col className="userInformationContainer">
+              <Row className="photoContainer">
+                <img className="userPhoto" src={user.photo_url} alt="photos" />
+              </Row>
+              <Row className="userInformation">
+                <h3>
+                  {user.first_name}
+                  {user.last_name}
+                </h3>
+                <p>
+                  <RoomOutlinedIcon />
+                  {user.city}
+                  {user.state}
+                  {user.zip}
+                </p>
+                <p>
+                  <EmailOutlinedIcon />
+                  {user.email}
+                </p>
+              </Row>
+            </Col>
+          </Col>
+          <Col className="classScrollInformation" med={8}>
+            <Row className="scrollContainer">
+              <UserClasses />
             </Row>
           </Col>
-        </Col>
-        <Col className="classScrollInformation" med={8}>
-          <Row className="scrollContainer">
-            <UserClasses />
-          </Row>
-        </Col>
-        <h1 className="classHeader">Classes</h1>
-        <UserClasses />
+        </Row>
       </Container>
     );
   }
   return null;
 };
-
 export default UserInformation;
