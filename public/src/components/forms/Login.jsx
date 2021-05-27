f1d3c530bddcfedd59085c09fd133ad38b0f4f93
B1-sauce <hby940604@gmail.com>
2021-05-26 19:22:40 -0700

    implement upcoming course for home page

--- public/src/components/forms/Login.jsx
+++ public/src/components/forms/Login.jsx
@@ -2,6 +2,7 @@
 /* eslint-disable react/prop-types */
 import React, { useState, useEffect } from 'react';
 // import { Modal } from 'react-bootstrap';
+/* eslint-disable no-console */
 import Button from '@material-ui/core/Button';
 import Dialog from '@material-ui/core/Dialog';
 import DialogActions from '@material-ui/core/DialogActions';
